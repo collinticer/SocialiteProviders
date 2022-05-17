66ba7ce74722d28ac01d12dfbd8381e49eb98431
Collin Ticer <collin.ticer@gmail.com>
2022-05-17 14:55:14 -0500

    updated provider to fix bug we're experiencing
    updated provider to fix bug we're experiencing due to "grant_type must be supplied in body" type error from azure AD

--- src/Azure/Provider.php
+++ src/Azure/Provider.php
@@ -104,6 +104,17 @@ class Provider extends AbstractProvider
     }
 
     /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+            'scope'      => parent::formatScopes(parent::getScopes(), $this->scopeSeparator),
+        ]);
+    }
+
+    /**
      * Get the access token response for the given code.
      *
      * @param string $code
